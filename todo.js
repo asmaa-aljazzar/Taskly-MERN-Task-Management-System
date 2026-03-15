8792c5a30040aa1d90d17e89916d51d162e0e2a8
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-03-15 13:46:36 +0300

    initial commit

---
+++ todo.js
@@ -0,0 +1,9 @@
+// Todo
+/*
+** Create GitHub repo.
+* Work on branches.
+* Continue working on `./backend/models/TimeLog.js`.
+* Prepare Frontend.
+* Start With Authentication.
+* Review Models.
+*/
